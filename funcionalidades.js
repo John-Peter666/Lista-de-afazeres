49d1d0a6640464bb78efc9b75d5b9d8aa9e6c1c1
demonio verdadeiro da biblia <joaao_andrade@hotmail.com>
2025-09-21 20:33:41 -0300

    terminei o funcionamento e aparência da lista de afazeres

--- funcionalidades.js
+++ funcionalidades.js
@@ -11,8 +11,40 @@ function addTask() {
     let li = document.createElement('li');
     li.innerHTML = inputBox.value;
     taskList.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
 }
 inputBox.value = '';
+saveTasks();
 } 
 
 addTaskBtn.addEventListener('click', addTask);
+
+taskList.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveTasks();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveTasks();
+    }
+}, false);
+
+// Adiciona a funcionalidade de pressionar "Enter" para adicionar a tarefa
+inputBox.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+
+function saveTasks() {
+    localStorage.setItem('data', taskList.innerHTML);
+
+}
+
+function loadTasks() {
+    taskList.innerHTML = localStorage.getItem('data');
+}
+
+loadTasks();
